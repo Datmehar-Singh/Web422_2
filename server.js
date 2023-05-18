a1361ee0a907c1b5210614b6aa2232883e4e6f46
mayank-Pareek <mpareek365@gmail.com>
2023-05-17 23:01:37 -0400

    Add required dependencies

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 8080;
-
+const cors = require("cors");
+require("dotenv").config();
+app.use(cors());
+app.use(express.json());
 app.get("/", (req, res) => {
   res.json({ msg: "API Listening" });
 });
